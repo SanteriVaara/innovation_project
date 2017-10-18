98b9a22f36f404ffd7dccfea73673ad2ab975d1a
unknown <santtu.vaara@gmail.com>
2017-10-18 16:18:46 +0300

    uus

--- mysite/personal/models.py
+++ mysite/personal/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Post(models.Model):
+    title = models.CharField(max_length = 140)
+    firstname = models.CharField(max_length = 50)
+    lastname = models.CharField(max_length =50)
+    file = models.FileField(upload_to='\personal\static\personal\images/%Y/%m/%d/')
+    date = models.DateTimeField(auto_now_add=True)
 
+    def __unicode__(self):
+        return self.title
--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -2,8 +2,20 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 def index(request):
 	return render(request, 'personal/index.html')
 	
-# Create your views here.
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'personal/index.html', {
+        'form': form
+    })
