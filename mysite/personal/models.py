7bb4b3cd4364c349c77ab8e480994cd6da0352ec
unknown <santtu.vaara@gmail.com>
2017-11-04 23:25:42 +0200

    uutta kontenttia

--- mysite/personal/models.py
+++ mysite/personal/models.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models
 from django.core.urlresolvers import reverse
+from django.db import models
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 class Post(models.Model):
     title = models.CharField(max_length = 140)
+    slug = models.SlugField(unique=True)
     content = models.TextField()
     firstname = models.CharField(max_length = 50)
     lastname = models.CharField(max_length =50)
@@ -21,4 +24,21 @@ class Post(models.Model):
 
     def get_absolute_url(self):
     	return reverse("personal:detail", kwargs={"id": self.id})
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug = slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug, qs.first().id)
+        return create_slug(instance, new_slug = new_slug)
+    return slug
+
+def pre_save_post_receiver(sender,instance,*args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
     	
--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from django.conf import settings
 from .forms import PostForm
@@ -9,9 +10,19 @@ from .models import Post
 
 def post_list(request):
     queryset_list = Post.objects.all().order_by("-date")
+
+    query = request.GET.get("Query")
+    if query:
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(firstname__icontains=query) |
+            Q(lastname__icontains=query)
+            ).distinct()
+
     paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
     page_request_var = "page"
-    page = request.GET.get(page_request_var)
+    page = request.GET.get(page_request_var) 
     try:
         queryset = paginator.page(page)
     except PageNotAnInteger:
@@ -28,6 +39,8 @@ def post_list(request):
     return render(request, 'post_list.html', context)
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -48,6 +61,8 @@ def post_detail(request, id=None):
     return render(request, 'post_detail.html', context)
 
 def post_update(request, id=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, id=id)
     form = PostForm(request.POST or None,  request.FILES or None, instance=instance)
     if form.is_valid():
@@ -64,6 +79,8 @@ def post_update(request, id=None):
     return render(request, 'post_form.html', context)
 
 def post_delete(request, id=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, id=id)
     instance.delete()
     return redirect("personal:list")
