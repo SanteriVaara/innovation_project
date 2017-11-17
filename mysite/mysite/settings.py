b1f3e31c8027414f986c84a718e412a290a52c7e
unknown <santtu.vaara@gmail.com>
2017-11-17 21:55:53 +0200

    perkele

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a6(e0$b++$l&4n#&wqkp-b!j%xc-n+=h&q93dj8dr#=zw$53oa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com","innoproject.herokuapp.com","127.0.0.1"]
 
