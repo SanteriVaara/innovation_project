10311585c1e3a4cdf01ac2a55bbce4a08e6ec246
unknown <santtu.vaara@gmail.com>
2017-11-17 21:13:22 +0200

    Procfile&settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a6(e0$b++$l&4n#&wqkp-b!j%xc-n+=h&q93dj8dr#=zw$53oa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['innoproject.herokuapp.com/']
 
 
 # Application definition
