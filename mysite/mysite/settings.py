ad1800c9fa31e4b765cf9acbbd1a122f78429725
unknown <santtu.vaara@gmail.com>
2017-11-28 21:28:01 +0200

    cas testaus

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -179,5 +179,5 @@ LOGGING = {
 }
 
 CAS_SERVER_URL = "https://idp.metropolia.fi/idp/profile/cas"
-CAS_LOGOUT_COMPLETELY = True
-CAS_PROVIDE_URL_TO_LOGOUT = True
+CAS_RETRY_LOGIN = True
+CAS_REDIRECT_URL = '/'
