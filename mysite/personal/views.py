c32234156a316995a926eeeb9053615a28ecb501
unknown <santtu.vaara@gmail.com>
2017-12-09 16:22:21 +0200

    korjaus äskeisen äskeiseen

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -70,15 +70,14 @@ def post_update(request, slug=None):
     if not request.user.is_authenticated():
         raise Http404
     instance = get_object_or_404(Post, slug=slug)
-    if not request.user == instance.user or not request.user.is_superuser:
-        raise Http404
-    form = PostForm(request.POST or None,  request.FILES or None, instance=instance)
-    if form.is_valid():
-        if request.user == instance.user or request.user.is_superuser:
+    if request.user == instance.user or request.user.is_superuser:
+       form = PostForm(request.POST or None,  request.FILES or None, instance=instance)
+       if form.is_valid():
            instance = form.save(commit=False)
            instance.save()
            return HttpResponseRedirect(instance.get_absolute_url())
-
+    elif not request.user == instance.user or not request.user.is_superuser:
+        raise Http404
     context = {
         "title": instance.title,
         "instance": instance,
