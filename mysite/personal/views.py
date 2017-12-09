47387ad59c3318696ccea56ac04c5eefc80d09a3
unknown <santtu.vaara@gmail.com>
2017-12-09 16:11:55 +0200

    make it better2

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -73,7 +73,7 @@ def post_update(request, slug=None):
     if not request.user == instance.user or not request.user.is_superuser:
     	raise Http404
     form = PostForm(request.POST or None,  request.FILES or None, instance=instance)
-    if form.is_valid():
+    if form.is_valid() and (request.user == instance.user or request.user.is_superuser):
         instance = form.save(commit=False)
         instance.save()
         return HttpResponseRedirect(instance.get_absolute_url())
